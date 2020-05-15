c358576b230f197b4806c86cf9b05abc76704fb3
Andrew Galloway <akq.galloway@gmail.com>
2020-05-14 19:37:43 -0700

    adds useful help message

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,14 @@ module.exports = {
   name: 'help',
   description: 'Help!',
   execute(message, rolls, sides) {
-    message.channel.send('Insert help message here');
+    message.channel.send('**Hi, I\'m Ico!**\n \
+    Try rolling a D20 using the following command: `!ico d20`\n \
+    You can roll more than one at a time too: `!ico 2d20`\n \
+    You can roll any number of sides you want: `!ico 3d42`\n \
+    \n \
+    I also support high-variance dice:  `!ico hv20`\n \
+    ... *but only with the following side counts:* 4, 6, 8, 10, 12, 20, 100\n \
+    \n \
+    To see this message again, just ask for help: `!ico help`');
   }
 };
