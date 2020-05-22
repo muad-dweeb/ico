b818fa7a3b25c7c3228b9706cea69d865d62a928
Andrew Galloway <akq.galloway@gmail.com>
2020-05-21 20:26:50 -0700

    adds logrotate configuration

--- ops/setup.sh
+++ ops/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 logfile=/var/log/ico.log
+logrotate_conf=/etc/logrotate.d/ico
 
 sudo apt update
 curl -sL https://deb.nodesource.com/setup_12.x -o /tmp/nodesource_setup.sh
@@ -9,5 +10,8 @@ sudo apt install -y nodejs
 
 sudo touch $logfile
 sudo chown ubuntu:ubuntu $logfile
+
+sudo cp ${HOME}/ico/ops/logrotate.conf $logrotate_conf
+sudo chown ubuntu:ubuntu $logrotate_conf
 
 npm i
