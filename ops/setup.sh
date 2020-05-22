8090750bb1d7605ddd2f6e302e9d1416af92e1cc
Andrew Galloway <akq.galloway@gmail.com>
2020-05-21 20:15:38 -0700

    adds runner script and logfile init

--- ops/setup.sh
+++ ops/setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+logfile=/var/log/ico.log
+
 sudo apt update
 curl -sL https://deb.nodesource.com/setup_12.x -o /tmp/nodesource_setup.sh
 sudo bash /tmp/nodesource_setup.sh
 sudo apt install -y nodejs
+
+sudo touch $logfile
+sudo chown ubuntu:ubuntu $logfile
 
 npm i
