3eed9273ead5b35f4e5733db2b909ad3f6edbb51
Andrew Galloway <akq.galloway@gmail.com>
2020-09-14 17:09:44 -0700

    dynamic user for permission setting

--- ops/setup.sh
+++ ops/setup.sh
@@ -2,6 +2,7 @@
 
 logfile=/var/log/ico.log
 logrotate_conf=/etc/logrotate.d/ico
+user=$(whoami)
 
 sudo apt update
 curl -sL https://deb.nodesource.com/setup_12.x -o /tmp/nodesource_setup.sh
@@ -9,9 +10,9 @@ sudo bash /tmp/nodesource_setup.sh
 sudo apt install -y nodejs
 
 sudo touch $logfile
-sudo chown ubuntu:ubuntu $logfile
+sudo chown $user:$user $logfile
 
 sudo cp ${HOME}/ico/ops/logrotate.conf $logrotate_conf
-sudo chown ubuntu:ubuntu $logrotate_conf
+sudo chown $user:$user $logrotate_conf
 
 npm i
