59793951ad9d398ada162353bfd7fc9c4f9946e0
Andrew Galloway <agalloway@coppertreeanalytics.com>
2021-09-09 22:07:21 -0700

    adds changelog command

---
+++ commands/changelog.js
@@ -0,0 +1,39 @@
+// Read from changelog.txt and format into a pretty message
+
+const {version} = require('../package.json');
+const {embedColor} = require('../config.json');
+const {homepage} = require('../package.json');
+
+const path = require('path')
+const fs = require('fs')
+
+const Discord = require('discord.js');
+
+const header = `New in version ${version}!`
+
+function simpleReadFileSync(filename)
+{
+    const filepath = path.resolve(filename)
+    var options = {encoding:'utf-8', flag:'r'};
+    var buffer = fs.readFileSync(filename, options);
+    return buffer
+}
+
+const contents = simpleReadFileSync('changelog.txt')
+const gitRoot = homepage.split('#')[0];
+
+module.exports = {
+  name: 'changelog',
+  description: 'Changelog',
+  cooldown: 20,
+  aliases: ['change', 'changes', 'c'],
+  execute: (message, args) => {
+    const changelogEmbed = new Discord.MessageEmbed()
+      .setColor(embedColor)
+      .setTitle(header)
+      .setThumbnail(gitRoot + '/blob/master/images/icon.png?raw=true')
+      .setDescription(contents)
+    console.log(contents)
+    message.channel.send(changelogEmbed);
+  }
+}
