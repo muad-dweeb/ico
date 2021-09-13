bdb6f824ab43b09a590fee79899e3bb9568d7fe9
Andrew Galloway <agalloway@coppertreeanalytics.com>
2021-09-12 21:20:35 -0700

    adds mongodb client

--- modules/clients.js
+++ modules/clients.js
@@ -1,14 +1,18 @@
 const Discord = require('discord.js');
 
+const { mongo } = require('../config.json');
+const { MongoClient } = require('mongodb');
+
 // fs is Node's native file system module
 const fs = require('fs');
 const path = require('path')
+
 
 /**
  * Initialize and set up a fresh Discord client
  * @returns {Discord.Client}
  */
-exports.discord = function () {
+exports.discord = () => {
   // create a new Discord client
   client = new Discord.Client();
   // an extension of JS's native Map class
@@ -33,4 +37,15 @@ exports.discord = function () {
     console.log('Ready!');
   });
   return client
+}
+
+/**
+ * Initialize and set up a fresh MongoDB client
+ * @returns {MongoClient}
+ */
+exports.mongo = () => {
+  // Initialize Atlas connection
+  const uri = `mongodb+srv://${mongo.user}:${mongo.pass}@ico-cluster.fgm2k.mongodb.net/icodb?retryWrites=true&w=majority`;
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  return client;
 }
